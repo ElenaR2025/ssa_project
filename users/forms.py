20f4fac4b7767e7ce182ca35feefe33865ad5745
unknown <elena.russell@outlook.com>
2024-11-12 17:20:18 +1100

    Chat Creation Week 5

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
@@ -26,3 +27,7 @@ class UserRegistrationForm(UserCreationForm):
             profile.nickname = self.cleaned_data['nickname']
             profile.save()
         return user
+    
+
+
+
